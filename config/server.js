a9c63e91bc2b5e70a1a8a827b31e358e702edaeb
unknown <jsmerchan@stevens.edu>
2021-04-14 11:50:36 +0530

    Initial Commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'fcf40df29deb954fc26587bde46de1e3'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '61c668f0-35bf-4eb5-ae92-8fc9df095750'
+};
